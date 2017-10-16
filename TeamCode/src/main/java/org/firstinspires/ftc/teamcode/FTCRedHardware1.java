d881c371d5cadd744eeb3b02e2771b0f0db232ba
Nate McKelvey <luv2sk8board@live.com>
2017-10-16 10:08:52 -0500

    Cleaned up Hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCRedHardware1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCRedHardware1.java
@@ -11,34 +11,30 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class FTCRedHardware1 {
 
-/**
- * This is NOT an opmode.
- *
- * This class can be used to define all the specific hardware for a single robot.
- * In this case that robot is a Pushbot.
- * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
- *
- * This hardware class assumes the following device names have been configured on the robot:
- * Note:  All names are lower case and some have single spaces between words.
- *
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
- * Servo channel:  Servo to open left claw:  "left_hand"
- * Servo channel:  Servo to open right claw: "right_hand"
- */
+    /**
+     * This is NOT an opmode.
+     *
+     * This class can be used to define all the specific hardware for a single robot.
+     * In this case that robot is a FTC RED'S SICK MECANUM DRIVEN DEATHMACHINE.
+     * See your mom and other classes starting with "Not A Phase" for usage examples.
+     *
+     * This hardware class assumes the following device names have been configured on the robot:
+     * Note:  All names are lower case and some have single spaces between words.
+     *
+     * Motor channel:  Left  drive motor:        "left_drive"
+     * Motor channel:  Right drive motor:        "right_drive"
+     * Motor channel:  Manipulator drive motor:  "left_arm"
+     * Servo channel:  Servo to open left claw:  "left_hand"
+     * Servo channel:  Servo to open right claw: "right_hand"
+     */
 
     /* Public OpMode members. */
     public DcMotor frontLeftMotor   = null;
-    public DcMotor  frontRightMotor  = null;
-    public DcMotor  rearLeftMotor   = null;
-    public DcMotor  rearRightMotor  = null;
-    public DcMotor  liftMotor = null;
-    public DcMotor  testMotor = null;
-    public Servo leftClaw = null;
-    public Servo rightCLaw = null;
-    public Servo rightBeaconClaw = null;
-    public Servo leftBeaconClaw = null;
+    public DcMotor frontRightMotor  = null;
+    public DcMotor rearLeftMotor   = null;
+    public DcMotor rearRightMotor  = null;
+    public DcMotor testMotor1 = null;
+    public DcMotor testMotor2 = null;
     public static final double CLOSE_SERVO       =  0.6 ;
     public static final double MID_SERVO       =  0.0 ;
     public static final double OPEN_SERVO       =  1 ;
@@ -60,27 +56,19 @@ public class FTCRedHardware1 {
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        frontLeftMotor   = hwMap.dcMotor.get("left_drive");
-        frontRightMotor  = hwMap.dcMotor.get("right_drive");
-        rearLeftMotor   = hwMap.dcMotor.get("lleft_drive");
-        rearRightMotor  = hwMap.dcMotor.get("rright_drive");
-        liftMotor  = hwMap.dcMotor.get("lift_drive");
-        testMotor  = hwMap.dcMotor.get("test_drive");
-        leftClaw = hwMap.servo.get("left_claw");
-        leftClaw.setPosition(OPEN_SERVO);
-        rightCLaw = hwMap.servo.get("right_claw");
-        rightCLaw.setPosition(MID_SERVO);
-        rightBeaconClaw = hwMap.servo.get("right_bc");
-        rightBeaconClaw.setPosition(MID_SERVO);
-        leftBeaconClaw = hwMap.servo.get("left_bc");
-        leftBeaconClaw .setPosition(CLOSE_SERVO);
+        frontLeftMotor   = hwMap.dcMotor.get("fleft");
+        frontRightMotor  = hwMap.dcMotor.get("fright");
+        rearLeftMotor   = hwMap.dcMotor.get("rleft");
+        rearRightMotor  = hwMap.dcMotor.get("rright");
+        testMotor1  = hwMap.dcMotor.get("lift_drive");
+        testMotor2  = hwMap.dcMotor.get("test_drive");
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         rearLeftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         rearRightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        liftMotor.setDirection(DcMotor.Direction.FORWARD);
-        testMotor.setDirection(DcMotor.Direction.FORWARD);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        testMotor1.setDirection(DcMotor.Direction.FORWARD);
+        testMotor2.setDirection(DcMotor.Direction.FORWARD);
+        testMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         /*Old motor settings
         frontLeftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -92,8 +80,8 @@ public class FTCRedHardware1 {
         frontRightMotor.setPower(0);
         rearLeftMotor.setPower(0);
         rearRightMotor.setPower(0);
-        liftMotor.setPower(0);
-        testMotor.setPower(0);
+        testMotor1.setPower(0);
+        testMotor2.setPower(0);
 
 
         // Set all motors to run without encoders.
@@ -102,8 +90,8 @@ public class FTCRedHardware1 {
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rearRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        testMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        testMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        testMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         // Define and initialize ALL installed servos.
 
     }
@@ -133,4 +121,15 @@ public class FTCRedHardware1 {
         period.reset();
     }
 }
+  /*
+
+        leftClaw = hwMap.servo.get("left_claw");
+        leftClaw.setPosition(OPEN_SERVO);
+        rightCLaw = hwMap.servo.get("right_claw");
+        rightCLaw.setPosition(MID_SERVO);
+        rightBeaconClaw = hwMap.servo.get("right_bc");
+        rightBeaconClaw.setPosition(MID_SERVO);
+        leftBeaconClaw = hwMap.servo.get("left_bc");
+        leftBeaconClaw .setPosition(CLOSE_SERVO);
+        */
 
